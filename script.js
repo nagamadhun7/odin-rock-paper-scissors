94021d0e88e0864c429cfc724e83bea97c535680
nagamadhun7 <nadakudurunagamadhu@gmail.com>
2024-12-25 23:34:53 -0500

    Add playRound function and score variables to store scores for human and computer

--- script.js
+++ script.js
@@ -1,14 +1,25 @@
+
+let humanScore = 0
+let computerScore = 0
+
 function getComputerChoice(){
     const games = ['rock','paper','scissor']
     let index = Math.floor(Math.random() * (2 - 0 + 1)) + 0;
     return games[index]
       }
     
-// console.log(getComputerChoice())
 
 function getHumanChoice(){
     let sign = prompt("What's your choice? rock, paper, scissor?");
-    console.log(sign)
+    return sign;
 }
+
+const humanSelection = getHumanChoice()
+const computerSelection = getComputerChoice()
       
-getHumanChoice()
+function playRound(humanChoice, computerChoice){
+    console.log(humanChoice, computerChoice)
+}
+
+
+playRound(humanSelection, computerSelection)
