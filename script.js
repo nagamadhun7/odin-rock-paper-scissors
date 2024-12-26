ed1256124a11307e2172fa9580e719f87d6724d7
nagamadhun7 <nadakudurunagamadhu@gmail.com>
2024-12-25 23:24:24 -0500

    Add function for computer choice

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
-console.log('Hello World!')
+function getComputerChoice(){
+    const games = ['rock','paper','scissor']
+    let index = Math.floor(Math.random() * (2 - 0 + 1)) + 0;
+    return games[index]
+      }
+    
+console.log(getComputerChoice())
+      
